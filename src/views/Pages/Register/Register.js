f15c09e9b68a8f2ae76c5510f35898b51d3cfdba
kavindadilshan <kavindadilshan97@gmail.com>
2023-06-30 21:58:16 +0530

    added user register api

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -15,18 +15,8 @@ import Loader from "../../../components/Loader/loading";
 
 class Register extends Component {
   state = {
-    fullName: {
-      value: '',
-      valid: false
-    },
-    email: {
-      value: '',
-      valid: false
-    },
-    password: {
-      value: '',
-      valid: ''
-    },
+    email: '',
+    password: '',
     loading: false
   }
 
@@ -35,18 +25,24 @@ class Register extends Component {
   }
 
   onTextChange = (event) => {
-    console.log(event)
-    // let name = event.target.name;
-    // let item = this.state[name];
-    // item.value = event.target.value;
-    // item.valid = true;
-    // this.setState({
-    //   [name]: item,
-    // });
+    let name = event.target.name;
+    this.setState({
+      [name]: event.target.value,
+    });
   }
 
-  handleSubmit = (e) => {
-
+  handleSubmit = async(e) => {
+    const obj = {
+      email:this.state.email,
+      password: this.state.password
+    }
+    await authService.registerUser(obj)
+      .then(res=>{
+        console.log(res)
+      })
+      .catch(err=>{
+        console.log(err)
+      })
   }
 
   render() {
@@ -58,20 +54,17 @@ class Register extends Component {
             <Col md="5" className="auth-container">
               <h2>Register</h2>
               <form className="register-form" onSubmit={this.handleSubmit}>
-                <label htmlFor="name">Full name</label>
-                <input value={fullName.value} name="name" onChange={(e) => this.onTextChange(e)} id="name"
-                       placeholder="full Name"/>
                 <label htmlFor="email">email</label>
                 <input value={email.value} onChange={(e) => this.onTextChange(e)} type="email"
                        placeholder="youremail@gmail.com" id="email" name="email"/>
                 <label htmlFor="password">password</label>
                 <input value={password.value} onChange={(e) => this.onTextChange(e)} type="password"
                        placeholder="********" id="password" name="password"/>
-                <button type="submit" className="button-auth">Sign Up</button>
+                <button type="button" className="button-auth" onClick={this.handleSubmit}>Sign Up</button>
               </form>
-              <Link to={BASE_URL + "/login"}>
-                <button className="link-btn">Already have an account? Login here.</button>
-              </Link>
+              {/*<Link to={BASE_URL + "/login"}>*/}
+              {/*  <button className="link-btn">Already have an account? Login here.</button>*/}
+              {/*</Link>*/}
             </Col>
           </Row>
         </Container>
