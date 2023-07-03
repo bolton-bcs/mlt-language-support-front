24b192810d8dd5c21d9a5dc94144efb39ba95041
kavindadilshan <kavindadilshan97@gmail.com>
2023-07-03 18:13:31 +0530

    ui improvements

--- src/views/PublicUser/ProductDetails/ProductDetails.js
+++ src/views/PublicUser/ProductDetails/ProductDetails.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import productImage from '../../../assets/img/icon/img.jpg';
 import './ProductDetails.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const ProductDetails = () => {
     return (
         <div className="container">
+          <center><h1>Place Your Order</h1></center>
             <div className="pview">
             <div className="row">
                 <div className="col-md-6">
@@ -18,52 +20,50 @@ const ProductDetails = () => {
                         <p className="unit-price">Featuring a sleek and timeless design, this bag is made from high-quality materials, ensuring durability and longevity. Its spacious interior offers ample room to carry all your essentials, from your wallet and phone to your cosmetics and even a small tablet. With multiple compartments and pockets, you can effortlessly organize your belongings and find them whenever you need them.</p>
                         <p className="unit-price">RS(Per Unit): 21999.99</p>
                     </div>
-                </div>
-
-
-                <form className='fm'>
-                <div className="row mb-3">
-                    <label htmlFor="quantity" className="col-sm-4 col-form-label">Quantity</label>
-                    <div className="col-sm-8">
+                  <form className='fm'>
+                    <div className="row mb-3">
+                      <label htmlFor="quantity" className="col-sm-4 col-form-label">Quantity</label>
+                      <div className="col-sm-8">
                         <input type="number" className="form-control" id="quantity" />
+                      </div>
                     </div>
-                </div>
-                <div className="row mb-3">
-                    <label htmlFor="price" className="col-sm-4 col-form-label">Price</label>
-                    <div className="col-sm-8">
+                    <div className="row mb-3">
+                      <label htmlFor="price" className="col-sm-4 col-form-label">Price</label>
+                      <div className="col-sm-8">
                         <input type="number" step="0.01" className="form-control" id="price" />
+                      </div>
                     </div>
-                </div>
-                <div className="row mb-3">
-                    <label htmlFor="country" className="col-sm-4 col-form-label">Country</label>
-                    <div className="col-sm-8">
+                    <div className="row mb-3">
+                      <label htmlFor="country" className="col-sm-4 col-form-label">Country</label>
+                      <div className="col-sm-8">
                         <select className="form-select" id="country">
-                            <option value="">Select a country</option>
-                            <option value="sl">Sri Lanka</option>
-                            <option value="us">United States</option>
-                            <option value="ca">Canada</option>
-                            <option value="uk">United Kingdom</option>
+                          <option value="">Select a country</option>
+                          <option value="sl">Sri Lanka</option>
+                          <option value="us">United States</option>
+                          <option value="ca">Canada</option>
+                          <option value="uk">United Kingdom</option>
                         </select>
+                      </div>
                     </div>
-                </div>
-                <div className="row mb-3">
-                    <label htmlFor="Delivery Address" className="col-sm-4 col-form-label">Delivery Address</label>
-                    <div className="col-sm-8">
+                    <div className="row mb-3">
+                      <label htmlFor="Delivery Address" className="col-sm-4 col-form-label">Delivery Address</label>
+                      <div className="col-sm-8">
                         <input type="text" step="0.01" className="form-control" id="Delivery_Address" />
+                      </div>
                     </div>
-                </div>
-                <div className="row mb-3">
-                    <label htmlFor="Expected_Date" className="col-sm-4 col-form-label">Expected Date</label>
-                    <div className="col-sm-8">
+                    <div className="row mb-3">
+                      <label htmlFor="Expected_Date" className="col-sm-4 col-form-label">Expected Date</label>
+                      <div className="col-sm-8">
                         <input type="date" step="0.01" className="form-control" id="Expected_Date" />
+                      </div>
                     </div>
+                    <div className="row">
+                      <div className="col-sm-12 text-end">
+                        <button type="submit" className="btn btn-primary">Request</button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
-                <div className="row">
-                <div className="col-sm-12 text-end">
-                  <button type="submit" className="btn btn-primary">Request</button>
-                </div>
-                </div>
-            </form>
 
             </div>
             </div>
